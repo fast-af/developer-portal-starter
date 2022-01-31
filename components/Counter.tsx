63d6aca9d880bccaa96b786126aed50c5e1c243d
tjh3 <thomas.harris@fast.co>
2022-01-31 06:15:47 -0800

    fixup! Initial Commit (not working)

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -1,16 +1,12 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
-import * as Router from "react-router-dom";
 
 export function Counter() {
   const [count, setCount] = React.useState(0);
-  let thecurrentPageLocation = Router.useLocation();
-  let thecurrentPageURL = thecurrentPageLocation.pathname;
 
   return (
     <div style={{ border: "1px solid red", padding: "10px" }}>
       <div style={{ fontSize: "18px", marginBottom: "10px" }}>
-        Path: {thecurrentPageURL}
+        Clicks: <strong>{count}</strong>
       </div>
       <button onClick={() => setCount(count + 1)}> Click </button>
     </div>
