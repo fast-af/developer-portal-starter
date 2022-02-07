4f7a6cf124bcafbf5bd945c2e38c59c61a7ad8e8
tjh3 <thomas.harris@fast.co>
2022-02-07 13:38:50 -0800

    DEVDOCS-107 attempt to use Router globalHistory instead of window.location

--- components/test.tsx
+++ components/test.tsx
@@ -1,35 +1,55 @@
 import * as React from "react";
-import { Button } from "@redocly/developer-portal/ui";
+import { render } from "react-dom";
+import { globalHistory } from "@reach/router";
+import { Button, Link } from "@redocly/developer-portal/ui";
+
+//let location = document.querySelector("link[rel='canonical']").href;
 
 export function Test() {
-  let oldStyleLocation = window.location.href;
-  let oldStylePath = window.location.pathname;
+  let ghBaseURL = "https://github.com/fast-af/devportal/edit";
+
+  //let oldStyleLocation = window.location.href;
+  let oldStyleLocation = globalHistory.location.href;
+  let baseURL = globalHistory.location.origin;
+  //let oldStylePath = window.location.pathname;
+  let oldStylePath = globalHistory.location.pathname;
+
+  let oldStyleHost = globalHistory.location.host;
+
+  let newPath = "";
   let branch = "";
   let buttonMessage = "";
 
+  const trimSlashes = (str) =>
+    str
+      .split("/")
+      .filter((v) => v !== "")
+      .join("/");
+
   if (oldStyleLocation.includes("preview.redoc.ly/fastaf")) {
-    oldStylePath = oldStylePath.replace("/fastaf/", "");
-    branch = oldStylePath.split("/")[0].replace("/", "");
+    newPath = oldStylePath.replace("/fastaf/", "");
+    newPath = trimSlashes(newPath);
+    branch = newPath.split("/")[0].replace("/", "");
+    branch = trimSlashes(branch);
     buttonMessage = "Edit Preview Branch on GitHub";
   } else if (oldStyleLocation.includes("fast.co/docs")) {
-    oldStylePath = oldStylePath.replace("/docs/", "master/");
+    newPath = oldStylePath.replace("/docs/", "");
+    newPath = trimSlashes(newPath);
+    branch = "master";
     buttonMessage = "Edit on GitHub";
-  } else if (oldStyleLocation.includes("localhost:")) {
+  } else if (oldStyleLocation.includes("localhost")) {
+    newPath = trimSlashes(oldStylePath);
+    branch = "REPLACE_ME_WITH_HYPHENATED_BRANCH_NAME";
     buttonMessage = "Local Dev Button CANNOT Link to GitHub";
-    oldStylePath = "REPLACE_ME_WITH_HYPHENATED_BRANCH_NAME" + oldStylePath;
   } else {
     buttonMessage = "SOMETHING WENT WRONG";
   }
 
-  let oldStylePathWithoutTrailingSlash = oldStylePath.substring(
-    0,
-    oldStylePath.length - (oldStylePath.endsWith("/") ? 1 : 0)
-  );
+  let ghEditLinkTest = ghBaseURL + "/" + branch + "/" + newPath + ".mdx";
 
-  let ghEditLink =
-    "https://github.com/fast-af/devportal/edit/" +
-    oldStylePathWithoutTrailingSlash +
-    ".mdx";
+  let ghNewEditLink = oldStyleLocation
+    .replace(baseURL, ghBaseURL)
+    .replace(oldStylePath, "/" + branch + "/" + newPath + ".mdx");
 
   return (
     <div style={{ border: "1px solid red", padding: "10px" }}>
@@ -39,7 +59,20 @@ export function Test() {
       <div style={{ fontSize: "18px", marginBottom: "10px" }}>
         Old Style PATH: <strong>{oldStylePath}</strong>
       </div>
-      <Button to={ghEditLink}>{buttonMessage}</Button>
+      <div style={{ fontSize: "18px", marginBottom: "10px" }}>
+        base URL: <strong>{baseURL}</strong>
+      </div>
+      <div style={{ fontSize: "18px", marginBottom: "10px" }}>
+        new Trimmed Path: <strong>{newPath}</strong>
+      </div>
+      <div style={{ fontSize: "18px", marginBottom: "10px" }}>
+        Original GitHub link: <strong>{ghEditLinkTest}</strong>
+      </div>
+      <div style={{ fontSize: "18px", marginBottom: "10px" }}>
+        New GitHub link: <strong>{ghNewEditLink}</strong>
+      </div>
+      <Button to={ghEditLinkTest}>{buttonMessage}</Button>
+      <Button to={ghNewEditLink}>{buttonMessage}</Button>
     </div>
   );
 }
